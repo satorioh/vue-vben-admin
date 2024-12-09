6321166170749caf61d453c64bbae74adc75498d
Robin.Wang <3Jg$94sbQ@q1>
2024-12-09 15:01:15 +0800

    feat(PictorialBar): add blue chart

--- src/views/demo/comp/pictorialBar/index.vue
+++ src/views/demo/comp/pictorialBar/index.vue
@@ -1,6 +1,7 @@
 <template>
   <div class="charts">
     <div class="chart" ref="chartRef"></div>
+    <div class="chart" ref="chartRef2"></div>
   </div>
 </template>
 
@@ -11,12 +12,14 @@
   defineOptions({ name: 'PictorialBar' });
 
   const chartRef = ref<HTMLElement | null>(null);
+  const chartRef2 = ref<HTMLElement | null>(null);
   const { setOptions } = useECharts(chartRef as Ref<HTMLDivElement>);
+  const { setOptions: setOptions2 } = useECharts(chartRef2 as Ref<HTMLDivElement>);
 
-  const value = 15;
+  const value = 45;
   const maxValue = 100;
 
-  const initChart = () => {
+  const initGreenChart = () => {
     setOptions({
       backgroundColor: '#0e202d',
 
@@ -62,7 +65,7 @@
               value: value,
               symbolPosition: 'end',
               itemStyle: {
-                color: 'rgba(0, 255, 136, 0.5)', //圆柱顶部颜色
+                color: 'rgba(126, 172, 119, 0.5)', //圆柱顶部颜色
               },
             },
           ],
@@ -81,7 +84,7 @@
               name: '',
               value: value,
               itemStyle: {
-                color: 'rgba(0, 255, 136, 1)', //圆柱底部颜色
+                color: 'rgba(126, 172, 119, 1)', //圆柱底部颜色
               },
             },
           ],
@@ -106,11 +109,11 @@
                   colorStops: [
                     {
                       offset: 0,
-                      color: 'rgba(0, 255, 136, 0)',
+                      color: 'rgba(126, 172, 119, 0)',
                     },
                     {
                       offset: 1,
-                      color: 'rgba(0, 255, 136, 1)', //底部渐变颜色
+                      color: 'rgba(126, 172, 119, 1)', //底部渐变颜色
                     },
                   ],
                 },
@@ -136,7 +139,7 @@
               value: maxValue,
               symbolPosition: 'end',
               itemStyle: {
-                color: 'rgba(0, 255, 136, 0.1)', //圆柱顶部颜色
+                color: 'rgba(126, 172, 119, 0.1)', //圆柱顶部颜色
               },
             },
           ],
@@ -156,7 +159,7 @@
               value: maxValue,
 
               itemStyle: {
-                color: 'rgba(0, 255, 136, .1)', //圆柱底部颜色
+                color: 'rgba(126, 172, 119, .1)', //圆柱底部颜色
               },
             },
           ],
@@ -187,23 +190,23 @@
                   colorStops: [
                     {
                       offset: 0,
-                      color: 'rgba(0, 255, 136, 0)',
+                      color: 'rgba(126, 172, 119, 0)',
                     },
                     {
                       offset: 0.3,
-                      color: 'rgba(0, 255, 136, .1)',
+                      color: 'rgba(126, 172, 119, .1)',
                     },
                     {
                       offset: 0.5,
-                      color: 'rgba(0, 255, 136, .1)',
+                      color: 'rgba(126, 172, 119, .1)',
                     },
                     {
                       offset: 0.8,
-                      color: 'rgba(0, 255, 136, .1)',
+                      color: 'rgba(126, 172, 119, .1)',
                     },
                     {
                       offset: 1,
-                      color: 'rgba(0, 255, 136, 0)', //底部渐变颜色
+                      color: 'rgba(126, 172, 119, 0)', //底部渐变颜色
                     },
                   ],
                 },
@@ -215,7 +218,209 @@
     });
   };
 
-  onMounted(() => initChart());
+  const initBlueChart = () => {
+    setOptions2({
+      backgroundColor: '#0e202d',
+
+      grid: {
+        top: 200,
+        bottom: 150,
+      },
+      xAxis: {
+        data: [],
+        axisTick: {
+          show: false,
+        },
+        axisLine: {
+          show: false,
+        },
+      },
+      yAxis: {
+        splitLine: {
+          show: false,
+        },
+        axisTick: {
+          show: false,
+        },
+        axisLine: {
+          show: false,
+        },
+        axisLabel: {
+          show: false,
+        },
+      },
+      series: [
+        // 内-顶部
+        {
+          name: '',
+          type: 'pictorialBar',
+          symbol: 'diamond',
+          symbolSize: [100, 45],
+          symbolOffset: [-10, -20],
+          z: 12,
+          data: [
+            {
+              name: '',
+              value: value,
+              symbolPosition: 'end',
+              itemStyle: {
+                color: 'rgba(136, 162, 255, 0.5)', //圆柱顶部颜色
+              },
+            },
+          ],
+        },
+
+        // 内-底部
+        {
+          name: '',
+          type: 'pictorialBar',
+          symbol: 'diamond',
+          symbolSize: [100, 45],
+          symbolOffset: [-10, 24],
+          z: 12,
+          data: [
+            {
+              name: '',
+              value: value,
+              itemStyle: {
+                color: 'rgba(136, 162, 255, 1)', //圆柱底部颜色
+              },
+            },
+          ],
+        },
+
+        // 内-渐变色
+        {
+          type: 'bar',
+          barWidth: 100,
+          data: [
+            {
+              name: '',
+              value: value,
+              itemStyle: {
+                color: {
+                  x: 0,
+                  y: 0,
+                  x2: 0,
+                  y2: 1,
+                  type: 'linear',
+                  global: false,
+                  colorStops: [
+                    {
+                      offset: 0,
+                      color: 'rgba(136, 162, 255, 0)',
+                    },
+                    {
+                      offset: 1,
+                      color: 'rgba(136, 162, 255, 1)', //底部渐变颜色
+                    },
+                  ],
+                },
+              },
+            },
+          ],
+        },
+
+        //往上是内部柱状图
+        //往下是外部柱状图
+
+        // 外-顶部
+        {
+          name: '',
+          type: 'pictorialBar',
+          symbol: 'diamond',
+          symbolSize: [140, 45],
+          symbolOffset: [-10, -20],
+          z: 12,
+          data: [
+            {
+              name: '',
+              value: maxValue,
+              symbolPosition: 'end',
+              itemStyle: {
+                color: 'rgba(136, 162, 255, 0.1)', //圆柱顶部颜色
+              },
+            },
+          ],
+        },
+
+        // 外-底部
+        {
+          name: '',
+          type: 'pictorialBar',
+          symbol: 'diamond',
+          symbolSize: [150, 75],
+          symbolOffset: [-10, 41],
+          z: 12,
+          data: [
+            {
+              name: '',
+              value: maxValue,
+
+              itemStyle: {
+                color: 'rgba(136, 162, 255, .1)', //圆柱底部颜色
+              },
+            },
+          ],
+        },
+
+        // 外-渐变色
+        {
+          type: 'bar',
+          silent: true,
+          barWidth: 140,
+          barGap: '-120%',
+          data: [
+            {
+              name: '',
+              value: maxValue,
+
+              label: {
+                show: false,
+              },
+              itemStyle: {
+                color: {
+                  x: 1,
+                  y: 1,
+                  x2: 1,
+                  y2: 0,
+                  type: 'linear',
+                  global: false,
+                  colorStops: [
+                    {
+                      offset: 0,
+                      color: 'rgba(136, 162, 255, 0)',
+                    },
+                    {
+                      offset: 0.3,
+                      color: 'rgba(136, 162, 255, .1)',
+                    },
+                    {
+                      offset: 0.5,
+                      color: 'rgba(136, 162, 255, .1)',
+                    },
+                    {
+                      offset: 0.8,
+                      color: 'rgba(136, 162, 255, .1)',
+                    },
+                    {
+                      offset: 1,
+                      color: 'rgba(136, 162, 255, 0)', //底部渐变颜色
+                    },
+                  ],
+                },
+              },
+            },
+          ],
+        },
+      ],
+    });
+  };
+
+  onMounted(() => {
+    initGreenChart();
+    initBlueChart();
+  });
 </script>
 
 <style scoped lang="less">
@@ -224,6 +429,7 @@
     height: 100%;
     position: relative;
     background: black;
+    display: flex;
 
     .chart {
       width: 50%;
