b83ad537d5e7c6ab951216172e5cceba153f9374
Robin.Wang <3Jg$94sbQ@q1>
2025-09-01 14:16:28 +0800

    feat(wx-ocr): add test image

--- src/views/demo/feat/weixin-ocr/index.vue
+++ src/views/demo/feat/weixin-ocr/index.vue
@@ -1,30 +1,44 @@
 <template>
   <div class="wx-ocr-demo-page">
-    <el-image
-      style="width: 100px; height: 100px"
-      :src="url"
-      :preview-src-list="srcList"
-      preview-teleported
-      fit="cover"
-    />
+    <div class="preview-container">
+      <el-image style="width: 100%; height: 100%" :src="blobUrl" preview-teleported fit="contain" />
+    </div>
   </div>
 </template>
 
 <script setup lang="ts">
+  import InvoiceImage from '@/assets/images/ocr/invoice.jpg';
+  import { ref, onMounted } from 'vue';
+
   defineOptions({
     name: 'WeiXinOcrDemo',
   });
 
-  const url = 'https://fuss10.elemecdn.com/a/3f/3302e58f9a181d2509f3dc0fa68b0jpeg.jpeg';
-  const srcList = [
+  const blobUrl = ref('');
+  const previewList = [
     'https://fuss10.elemecdn.com/a/3f/3302e58f9a181d2509f3dc0fa68b0jpeg.jpeg',
     'https://fuss10.elemecdn.com/1/34/19aa98b1fcb2781c4fba33d850549jpeg.jpeg',
-    'https://fuss10.elemecdn.com/0/6f/e35ff375812e6b0020b6b4e8f9583jpeg.jpeg',
-    'https://fuss10.elemecdn.com/9/bb/e27858e973f5d7d3904835f46abbdjpeg.jpeg',
-    'https://fuss10.elemecdn.com/d/e6/c4d93a3805b3ce3f323f7974e6f78jpeg.jpeg',
-    'https://fuss10.elemecdn.com/3/28/bbf893f792f03a54408b3b7a7ebf0jpeg.jpeg',
-    'https://fuss10.elemecdn.com/2/11/6535bcfb26e4c79b48ddde44f4b6fjpeg.jpeg',
   ];
+
+  const imageToBlobUrl = async (image) => {
+    const resp = await fetch(image);
+    const blob = await resp.blob();
+    return URL.createObjectURL(blob);
+  };
+
+  onMounted(async () => {
+    blobUrl.value = await imageToBlobUrl(InvoiceImage);
+  });
 </script>
 
-<style scoped lang="less"></style>
+<style scoped lang="scss">
+  .wx-ocr-demo-page {
+    padding: 20px;
+    .preview-container {
+      width: 100%;
+      height: calc(100vh - 145px);
+      overflow: auto;
+      text-align: center;
+    }
+  }
+</style>
